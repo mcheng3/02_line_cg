2af23b45ca7c5d6d1906f02efafdfbd11fa7bba7
mcheng3 <michael.cheng65@gmail.com>
2018-02-09 15:36:35 -0500

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,12 +1,26 @@
 from display import *
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-    x = x0
-    y = y0
+def draw_line(x0,y0,x1,y1,screen,color):
+    if x0<x1:
+        x = x0
+    else:
+        x = x1
+        x1 = x0
+        x0 = x
+    if y0 < y1:
+        y = y0
+    else:
+        y = y1
+        y1 = y0
+        y0 = y
     a = y1 - y0
     b = x0 - x1
     d = 2*a + b
-    if a>0 and -b>0 and -b >= a:
+    print 'a: ' + str(a)
+    print '-b: ' + str(-b)
+    
+    if -b >= a:
+        print "octet I"
         while x<=x1:
             plot( screen, color, x, y)
             if d > 0:
@@ -14,7 +28,30 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 d += 2*b
             x += 1
             d += 2*a
-    elif a>0 and -b> 0 and -b < a:
-        while false:
-            pass
-    
+    elif -b < a:
+        print "octet II"
+        while y<=y1:
+            plot( screen, color, x, y)
+            if d < 0:
+                x += 1
+                d += 2*a
+            y += 1
+            d += 2*b
+    elif b < a:
+        print "octet III"
+        while y<=y1:
+            plot( screen, color, x, y)
+            if d > 0:
+                x -= 1
+                d -= 2*a
+            y += 1
+            d += 2*b
+    elif b >= a:
+        print "octet IV"
+        while x>=x1:
+            plot( screen, color, x, y)
+            if d < 0:
+                y += 1
+                d += 2*b
+            x -= 1
+            d -= 2*a
--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0, 0, 500, 250, screen, color)
+draw_line(0,0,500,250,screen,[255,0,255])
+draw_line(0,0,250,500,screen,color)
+draw_line(500,0,250,500,screen,[0,0,255])
+draw_line(500,0,0,250,screen,[255,255,0])
+#draw_line(500, 0, 400, 500, screen, [255,255,0])
+#draw_line(500, 0, 0, 10, screen, [255, 255, 255])
+#draw_line(50, 500, 0, 100, screen, [255,255,100])
 display(screen)
 save_extension(screen, 'img.png')
