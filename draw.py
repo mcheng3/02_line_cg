ec18ec30d13da35277b6ebc435a0e8cc844c3a1f
mcheng3 <michael.cheng65@gmail.com>
2018-02-09 11:47:39 -0500

    Complete octet I

--- draw.py
+++ draw.py
@@ -1,7 +1,20 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    i=0
-    while i<500:
-        plot( screen, color, i, i)
-        i += 1
+    x = x0
+    y = y0
+    a = y1 - y0
+    b = x0 - x1
+    d = 2*a + b
+    if a>0 and -b>0 and -b >= a:
+        while x<=x1:
+            plot( screen, color, x, y)
+            if d > 0:
+                y += 1
+                d += 2*b
+            x += 1
+            d += 2*a
+    else if a>0 and -b> 0 and -b < a:
+        while:
+    return    
+    
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0, 0, 0, 0, screen, color)
+draw_line(0, 0, 500, 250, screen, color)
 display(screen)
 save_extension(screen, 'img.png')
