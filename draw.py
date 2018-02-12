87c433238be626f639a579a3ba38ea877b3de2d3
mcheng3 <michael.cheng65@gmail.com>
2018-02-11 23:48:24 -0500

    Fix bug

--- draw.py
+++ draw.py
@@ -9,8 +9,8 @@ def draw_line(x0,y0,x1,y1,screen,color):
     if x0 > x1:       
         c = False
     if y0 > y1:
-        c = c == False
-    if x0 > x1 and y0 > y1 or x0 < x1 and y0 > y1:
+       c = c == False
+    if x0 > x1 and y0 > y1 or x0 <= x1 and y0 > y1:
         #print "swapping"
         x = x1
         x1 = x0
@@ -27,7 +27,7 @@ def draw_line(x0,y0,x1,y1,screen,color):
     #print '-b: ' + str(-b)
     #print "c: " + str(c)
     if c and math.fabs(b) >= math.fabs(a):
-        #print "octet I"
+        print "octet I"
         while x<=x1:
             plot( screen, color, x, y)
             if d > 0:
@@ -36,7 +36,7 @@ def draw_line(x0,y0,x1,y1,screen,color):
             x += 1
             d += 2*a
     elif c and math.fabs(b) < math.fabs(a):
-        #print "octet II"
+        print "octet II"
         while y<=y1:
             plot( screen, color, x, y)
             if d < 0:
@@ -45,8 +45,8 @@ def draw_line(x0,y0,x1,y1,screen,color):
             y += 1
             d += 2*b
     elif not c and math.fabs(b) < math.fabs(a):
-        #print "octet III"
-        #print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
+        print "octet III"
+        print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d)  + " " + str(y1)
         while y<=y1:
             #print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
             plot( screen, color, x, y)
@@ -56,7 +56,7 @@ def draw_line(x0,y0,x1,y1,screen,color):
             y += 1
             d += 2*b
     elif not c and math.fabs(b) >= math.fabs(a):
-        #print "octet IV"
+        print "octet IV"
         #print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
         while x>=x1:
             plot( screen, color, x, y)
