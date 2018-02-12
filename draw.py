2d2ca81a7b51e2df79f91e97c3baa72b74058da1
mcheng3 <michael.cheng65@gmail.com>
2018-02-11 22:54:18 -0500

    Finish work 02

--- draw.py
+++ draw.py
@@ -1,25 +1,32 @@
 from display import *
+import math
 
 def draw_line(x0,y0,x1,y1,screen,color):
-    if x0<x1:
-        x = x0
-    else:
+    c = True
+    x=x0
+    y=y0
+    
+    if x0 > x1:       
+        c = False
+    if y0 > y1:
+        c = c == False
+    if x0 > x1 and y0 > y1 or x0 < x1 and y0 > y1:
+        print "swapping"
         x = x1
         x1 = x0
         x0 = x
-    if y0 < y1:
-        y = y0
-    else:
         y = y1
         y1 = y0
         y0 = y
+
+
     a = y1 - y0
     b = x0 - x1
     d = 2*a + b
     print 'a: ' + str(a)
     print '-b: ' + str(-b)
-    
-    if -b >= a:
+    print "c: " + str(c)
+    if c and math.fabs(b) >= math.fabs(a):
         print "octet I"
         while x<=x1:
             plot( screen, color, x, y)
@@ -28,7 +35,7 @@ def draw_line(x0,y0,x1,y1,screen,color):
                 d += 2*b
             x += 1
             d += 2*a
-    elif -b < a:
+    elif c and math.fabs(b) < math.fabs(a):
         print "octet II"
         while y<=y1:
             plot( screen, color, x, y)
@@ -37,17 +44,20 @@ def draw_line(x0,y0,x1,y1,screen,color):
                 d += 2*a
             y += 1
             d += 2*b
-    elif b < a:
+    elif not c and math.fabs(b) < math.fabs(a):
         print "octet III"
+        print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
         while y<=y1:
+            print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
             plot( screen, color, x, y)
             if d > 0:
                 x -= 1
                 d -= 2*a
             y += 1
             d += 2*b
-    elif b >= a:
+    elif not c and math.fabs(b) >= math.fabs(a):
         print "octet IV"
+        print "x: " + str(x) + "  y: " + str(y) + "  d: " + str(d) 
         while x>=x1:
             plot( screen, color, x, y)
             if d < 0:
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
 
-draw_line(0,0,500,250,screen,[255,0,255])
-draw_line(0,0,250,500,screen,color)
-draw_line(500,0,250,500,screen,[0,0,255])
-draw_line(500,0,0,250,screen,[255,255,0])
-#draw_line(500, 0, 400, 500, screen, [255,255,0])
-#draw_line(500, 0, 0, 10, screen, [255, 255, 255])
-#draw_line(50, 500, 0, 100, screen, [255,255,100])
+
+draw_line(0,0,500,250,screen,[255,0,0])
+draw_line(0,0,250,500,screen,[255,75,0])
+draw_line(500,0,250,500,screen,[255,255,0])
+draw_line(500,0,0,250,screen,[0,255,0])
+draw_line(500, 500, 0, 250, screen, [0,255,255])
+draw_line(500, 500, 250, 0, screen, [0,0,255])
+draw_line(0, 500, 250, 0, screen, [145,0,255])
+draw_line(0, 500, 500, 250, screen, [255,0,255])
 display(screen)
 save_extension(screen, 'img.png')
